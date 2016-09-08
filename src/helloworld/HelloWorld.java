c46a3a34bc9f40c613b3c2dc4613e541f63c9385
grace <grace@grace-Lenovo-Y50-70>
2016-09-08 12:17:04 -0600

    Ints

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -29,6 +29,68 @@ public class HelloWorld {
         assert true !=false;
         assert true == true;
         assert false == false;
+    }   
+    
+    void testInts(){
+        assert Integer.MAX_VALUE == Math.pow(2,31)-1;
+        println(Integer.MAX_VALUE);
+        assert Integer.MAX_VALUE == 2_147_483_647;
+        assert Integer.MAX_VALUE == 0b0111_1111_111111111111111111111111;
+       
+        assert Integer.MIN_VALUE == -Math.pow(2,31);
+        println(Integer.MIN_VALUE);
+        assert Integer.MIN_VALUE == -2_147_483_648;
+        assert Integer.MIN_VALUE == 0b1000_0000_0000_00000000000000000000;
+       
+        assert Integer.MIN_VALUE - 1 == Integer.MAX_VALUE;
+        assert Integer.MAX_VALUE + 1 == Integer.MIN_VALUE;
+        
+        int x = 3;
+        int y = 0xff_ff;
+        int z = 0b1001;
+        
+        assert x ==3;
+        println (y);
+        assert y == 65_535;
+        assert z == 9;
+                //bitewise and
+        assert(0b1111_0000 & 0b1010_1010) == 0b1010_0000;
+        //bitwise or
+        assert(0b1111_0000 | 0b1010_1010) == 0b1111_1010;
+        //bitwise shift operator 
+        assert (0b1010_1111_0000 >> 4) == 0b1010_1111;
+        assert (0b1010_1111_0000 << 4) == 0b1010_1111_0000_0000;
+        
+        int b = random (0, 1_000_000);
+       
+        assert ((b >> 1) == b/2);
+        assert (((-b) >> 1) == -b/2);
+        assert ((b << 1) == 2*b);
+        assert (((-b)<<1) == 2*(-b));
+        
+        
+        println(b);
+        
+        int c = random(-1_000_000, 1_000_000);
+        assert ((~c)+1) == -c;
+        assert -2 == 0b1111_1111_1111_1111_1111_1111_1111_1110;
+        
+        //right shifts shift the sign bit in (highest order bit)
+        assert (-2 >> 1) == 0b1111_1111_1111_1111_1111_1111_1111_1111;
+        
+        //how do- you represent -2 in two's complement
+        println(Integer.toString(-2, 2));
+        
+        
+    }
+      
+    void testConvert(){
+        byte x = -1;
+        //casting notation
+        assert x == (byte) 0b1111_1111;
+        int y = x;
+        assert y == -1;
+    }
+
 }
     
-}
