6dc7690885544fd042ec33a6c9350b096a8d0979
grace <grace@grace-Lenovo-Y50-70>
2016-09-27 12:04:28 -0600

    equals

--- src/helloworld/Clock.java
+++ src/helloworld/Clock.java
@@ -10,7 +10,7 @@ import static kiss.API.*;
  *
  * @author grace
  */
-public class Clock {
+public class Clock extends java.lang.Object implements Comparable<Clock>{
     //instance variables
     private double hours=0;
     private double minutes=0;
@@ -43,6 +43,33 @@ public class Clock {
         return (_minutes-Math.floor(_minutes))*60.0;
     }
     
+     @Override
+    public int compareTo(Clock clock) {
+        double delta = getHours()-clock.getHours();
+        if(delta < 0) return -1;
+        if (delta == 0) return 0;
+        return 1;
+    }
+       
+    //overload of the definition of clock -- the types have to be different. 
+    public boolean equals(Clock clock){
+        return getHours() == clock.getHours(); // overloading allowed in Java
+    }
+    /*
+       public boolean equals(Clock specialclock){
+        return getHours() == specialclock.getHours(); // NOT ALLOWED 
+    }
+    */
+   @Override //safety for the compiler it overrides the word equal to give it a new definition 
+   //hint for us so that what we think is true is actually true at runtime
+   
+   
+   public boolean equals(Object object){
+           return (object instanceof Clock) && equals((Clock)object);
+    }
+   //compare clocks not are they equal but is one less than the other
+   
+   
     void testGetTime(){
         Clock clock = new Clock();
         double hours = clock.getHours();
@@ -66,4 +93,22 @@ public class Clock {
         double shouldBe = 1.0 + 1.0/3600.0;
         assert abs (now-shouldBe) < 0.1 / 3600.0;
     }
+  
+   
+   
+    void testEquals(){
+        Clock clock1 = new Clock();
+        Clock clock2 = clock1;
+        Clock clock3 = new Clock();
+        
+        clock1.setHours(1.00);
+        clock3.setHours(1.00);
+        assert clock2.getHours() == 1.00;
+        assert clock1.equals(clock2) == true;
+        assert clock1.equals(clock3) == true; // we overrode what equals means so in this instance == will not mean equal anymore
+        assert (clock1 == clock3) == false; //we cannot change the operator == are they referring to the same thing?
+        
+    }
+//Definition on how to compare clocks 
+   
 }
