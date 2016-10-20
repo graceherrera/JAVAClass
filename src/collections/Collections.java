5395554ddcf9e291a38f9e1a78682075c238473f
grace <grace@grace-Lenovo-Y50-70>
2016-10-20 11:30:36 -0600

    First Test

---
+++ src/collections/Collections.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+import java.util.ArrayList;
+import static kiss.API.*;
+/**
+ *
+ * @author grace
+ */
+public class Collections {
+    void testRun(){
+        outExpect("Hello Collections");
+        run();
+        outClose();
+    }
+    
+    void run(){
+        println("Hello Collections");
+    }
+    
+    void createCollection(){
+       ArrayList<Integer> applications = new ArrayList<Integer>();
+       
+       
+    }
+}
