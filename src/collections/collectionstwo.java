115c4a33200a860b9f34b32c542914df15f796a5
grace <grace@grace-Lenovo-Y50-70>
2016-11-12 11:12:09 -0700

    created arrays to test out

---
+++ src/collections/collectionstwo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+import java.lang.*;
+import java.util.*;
+
+/**
+ *
+ * @author grace
+ */
+public class collectionstwo {
+    
+    public static void main(String[] args){
+        int[] numbers = {5, 3, 17, 25, 4, 33, 45, 12};
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        boolean[] responses = {true, false, false, true, true};
+        boolean[] responses2 = {false, true, false, true, true};
+    }
+}
